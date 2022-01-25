0734680838ea1d77dde50b6ff61df72a8c732c14
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2022-01-25 12:15:19 +0000

    Add foreign key links in models file, still some errors left

--- booking/models.py
+++ booking/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from datetime import datetime, date, time, timedelta 
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Restaurant(models.Model):
+
+    name = models.CharField(max_length=50)
+    opening_time = models.TimeField(default=time(11, 00))
+    closing_time = models.TimeField(default=time(10, 00))
+    menu = CloudinaryField('image', default='placeholder', use_filename=True, help_text='image of restaurants menu')
+
+    def __str__(self):
+        return self.restaurant_name
+
 
 class Table(models.Model):
 
@@ -13,12 +25,12 @@ class Table(models.Model):
         (6, 'Table of 6'),
     ]
 
-    table_number = models.IntergerField(max_length=20)
-    size = models.IntergerField(choices=TABLE_SIZE)
-    resturant_name = models.CharField(max_length=50)
+    number = models.IntegerField(max_length=20)
+    size = models.IntegerField(choices=TABLE_SIZE)
+    restaurant_name = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True, related_name='name')
 
     def __str__(self):
-        return
+        return f"A table of {self.size} persons"
 
 
 class Booking(models.Model):
@@ -29,23 +41,28 @@ class Booking(models.Model):
         (3, '3 persons'),
         (4, '4 persons'),
         (5, '5 persons'),
-        (6, '6 persons').
+        (6, '6 persons'),
     ]
 
     booking_id = models.CharField(max_length=6, unique=True)
-    name = models.CharField(max_length=20, uniques=False)
-    party_size = models.IntergerField(choices=PARTY_SIZE, default=2)
+    customer_name = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='name')
+    party_size = models.IntegerField(choices=PARTY_SIZE, default=2)
     created_on = models.DateTimeField(auto_now=True)
     start_time = models.TimeField(auto_now=False, auto_now_add=False, default=time(12, 00))
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
-    phone_number = models.IntergerField(max_length=15, unique=True)
+    phone_number = models.IntegerField(max_length=15, unique=True)
     email = models.EmailField(max_length=250)
     date = models.DateField(default=date.today)
     updated_on = models.DateTimeField(auto_now=True)
-    table_number = models.ManyToManyField()
+    table_number = models.ManyToManyField(Table, related_name='number')
 
     class Meta:
         ordering = ['-created_on']
+
+    
+    def created_end_time(self):
+        end_time = (datetime.combine(date.today(), self.time)) + timedelta(hours=2)
+        return end_time.time()
     
     def __str__(self):
-        return
+        return f"A table of {self.party_size} on {datetime.strftime(self.date, '%d-%m-%y')}"
