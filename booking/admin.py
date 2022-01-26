b8470e0ae0602f7c0e725bdd82eaf2f15b723031
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2022-01-26 12:39:29 +0000

    Add admin registerations, created make_booking file

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Booking, Table, Restaurant
 
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Table)
+admin.site.register(Restaurant)
