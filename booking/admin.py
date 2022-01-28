854936e0ba5f1f64bfe8bdc4c1c0924b8b850a88
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2022-01-28 12:39:48 +0000

    Add options for admin panel in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -6,6 +6,7 @@ from .models import Booking, Table, Restaurant
 class BookingAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'date', 'party_size')
     search_fields = ['name']
+    list_filter = ('created_on', 'party_size')
     ordering = ('-date', '-start_time')
     actions = ['delete_selected']
     filter_horizontal = (
@@ -14,8 +15,12 @@ class BookingAdmin(admin.ModelAdmin):
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    pass
+    # list_display
+    ordering = ('size',)
+    list_filter = ('size',)
+    actions = ['delete_selected']
+
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'opening_time', 'closing_time', 'menu')
